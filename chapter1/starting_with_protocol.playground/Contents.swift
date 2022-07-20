0e509e4c1c031ea8a8fcb8de312fef8392b40c23
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2022-07-20 14:27:17 +0700

    designing with protocols

--- chapter1/starting_with_protocol.playground/Contents.swift
+++ chapter1/starting_with_protocol.playground/Contents.swift
@@ -252,3 +252,63 @@ var myDisplayName = MyDisplayName()
 var person1 = Person1(displayNameDelegate: myDisplayName)
 person1.firstName = "Tommy"
 person1.lastName = "Putra"
+
+// Designing with protocols
+//Robot
+protocol RobotMovement {
+    func forward(speedPercent: Double)
+    func reverse(speedPercent: Double)
+    func left(speedPercent: Double)
+    func right(speedPercent: Double)
+    func stop()
+}
+
+protocol RobotMovementThreeDimensions: RobotMovement {
+    func up(speedPercent: Double)
+    func down(speedPercent: Double)
+}
+
+//Sensor
+protocol Sensor {
+    var sensorType: String { get }
+    var sensorName: String { get set }
+    init(sensorName: String)
+    func pollSensor()
+}
+
+protocol EnvironmentSensor: Sensor {
+    func currentTemperature() -> Double
+    func currentHumidity() -> Double
+}
+
+protocol RangeSensor: Sensor {
+    func setRangeNotification(rangeCentimeter: Double, rangeNotification: () -> Void)
+    func currentRange() -> Double
+}
+
+protocol DisplaySensor: Sensor {
+    func displayMessage(message: String)
+}
+
+protocol WirelessSensor: Sensor {
+    func setMessageReceivedNotification(messageNotification:
+     (String) -> Void)
+     func messageSend(message: String)
+}
+
+protocol Robot {
+    var name: String {get set}
+     var robotMovement: RobotMovement {get set}
+     var sensors: [Sensor] {get}
+     init (name: String, robotMovement: RobotMovement)
+     func addSensor(sensor: Sensor)
+     func pollSensors()
+}
+
+
+
+
+
+
+
+
